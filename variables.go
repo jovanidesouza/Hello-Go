97c659787304c289ceafe0cb4a8de4b8723d422a
jovanidesouza <jovanidesouza@hotmail.com>
2020-08-17 21:55:11 -0300

    variables

---
+++ variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    var a = "string"
+	fmt.Println(a)
+	
+	b := "str" + "ing"
+    fmt.Println(b)
+
+    var c, d int = 0, 1
+	fmt.Println(c, d)
+	
+	var e float32 = 1 + 1
+    fmt.Println(e)
+
+    var f = true
+    fmt.Println(!f)
+
+}
